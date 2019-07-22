f5b76acdb7a0e882ad355bdf4493e337f1dc0477
FahturRahman Fauzi <fahturrahman18ti@mahasiswa.pcr.ac.id>
2019-07-22 14:57:48 +0700

    Pembuatan repo

---
+++ application/controllers/Auth.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email', [
+            'required' => 'Email wajib diisi!',
+            'valid_email' => 'Email tidak valid!'
+        ]);
+        $this->form_validation->set_rules('password', 'Password', 'trim|required', [
+            'required' => 'Password wajib diisi!'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $data['judul'] = 'Login';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $pass = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        if ($user) { } else {
+            $this->session->set_flashdata(
+                'message',
+                '<div class="alert alert-danger" role="alert">
+                    Email atau password salah!
+                </div>'
+            );
+            redirect('auth');
+        }
+    }
+
+    public function register()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim', [
+            'required' => 'Nama Wajib diisi!'
+        ]);
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'Email sudah terdaftar!',
+            'required' => 'Email Wajib diisi!',
+            'valid_email' => 'Email tidak valid!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]', [
+            'matches' => 'Password tidak sama!',
+            'min_length' => 'Password terlalu pendek!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['judul'] = 'Register';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/register');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'nama_user' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'gambar' => 'image.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata(
+                'message',
+                '<div class="alert alert-success" role="alert">
+                    Akun berhasil di daftar! Silahkan login
+                </div>'
+            );
+            redirect('auth');
+        }
+    }
+}
