cb231282e64e79bdeafcbcc47348796bfbc0764e
fahturr <fatur2077@gmail.com>
2019-08-03 17:30:28 +0700

    Penambahan SQL

---
+++ ktr.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 03, 2019 at 12:27 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ktr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL,
+  `nama_menu` varchar(64) NOT NULL,
+  `gambar_menu` varchar(64) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `deskripsi` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id_menu`, `nama_menu`, `gambar_menu`, `harga`, `deskripsi`) VALUES
+(1, 'Silver MW Catering', 'menu1.jpg', 5000000, 'Peralatan makan tersedia untuk 200 porsi\r\n\r\n'),
+(2, 'Gold MW Catering', 'menu2.jpg', 10000000, 'Peralatan makan tersedia untuk 300 porsi'),
+(3, 'Platinum MW Catering', 'menu3.jpg', 20000000, 'peralatan makan+pramusaji+400 porsi(diskon 15%)'),
+(4, 'Diamond MW Catering', 'menu4.jpg', 30000000, 'Peralatan makan+pramusaji+500 porsi(diskon 25%)'),
+(5, 'Pramusaji', 'menu5.jpg', 0, 'Khusus paket Platinum dan paket Diamond MW Catering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(128) NOT NULL,
+  `gambar` varchar(256) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `id_role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama_user`, `gambar`, `email`, `password`, `id_role`) VALUES
+(12, 'FahturRahman Fauzi', 'image.png', 'fahturrahman18ti@mahasiswa.pcr.ac.id', '$2y$10$f41/acvbwNEfF4xF3TXyDevJrJJAHQ6Mts393s2xdEA7eYMbxGHpG', 2),
+(14, 'Dwi Safitra', 'image.png', 'dwi18ti@mahasiswa.pcr.ac.id', '$2y$10$6WkBqsbhQUa2vyjPTYlmnu8Hckm9nyyUC0e6.4fxDq4Q6QHjxIts6', 1),
+(15, 'Nofiarly Kadarisman', 'image.png', 'nofiarly18ti@mahasiswa.pcr.ac.id', '$2y$10$v1cpMTFZaJU44VsSH22S..jBjHVzPf6d0the2x9TUANpjarJCt9x6', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id_role` int(11) NOT NULL,
+  `nama` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id_role`, `nama`) VALUES
+(1, 'user'),
+(2, 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_id_user` (`id_user`),
+  ADD KEY `fk_id_menu` (`id_menu`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `fk_id_role` (`id_role`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_id_menu` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`),
+  ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_id_role` FOREIGN KEY (`id_role`) REFERENCES `user_role` (`id_role`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
